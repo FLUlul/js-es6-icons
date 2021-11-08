f7342b69d63ee1fda72f6ccc601cef0f0f10866f
FLU <valerio.massj@gmail.com>
2021-11-08 17:10:03 +0100

    extra done with the type selector

--- js/script.js
+++ js/script.js
@@ -4,21 +4,37 @@ Partendo dalla struttura dati fornita, visualizzare in pagina un box per ogni ic
 
 Milestone 2
 Ciascuna icona ha una proprietà “color”: utilizzare questa proprietà per visualizzare le icone del colore corrispondente.
-*/
-const container = document.getElementById("container")
-console.log(icons);
 
+Bonus:
+Milestone 3
+Aggiungere alla pagina una select in cui le options corrispondono ai vari tipi di icone (animal, vegetable, user). Quando l’utente seleziona un tipo dalla select, visualizzare solamente le icone corrispondenti.
+(Qui quale potrebbe essere l’evento che mi serve?..)
+*/
+const containerGeneral = document.getElementById("container");
+const dropDownMenu = document.getElementById("mydropdown");
 
 icons.forEach((icon) => {
-    console.log(icon.name, icon.prefix, icon.type, icon.family, icon.color);
-    printHtml (icon.family, icon.prefix, icon.name, icon.color);
+    iconsPrintHtml (icon.family, icon.prefix, icon.name, icon.color, icon.type);
 });
 
+dropDownMenu.addEventListener("change",
+    function(){
+        containerGeneral.innerHTML = "";
+        console.log(dropDownMenu.value);
+        icons.forEach((icon) => {
+            if(dropDownMenu.value === icon.type){
+                iconsPrintHtml (icon.family, icon.prefix, icon.name, icon.color, icon.type);
+            }else if (dropDownMenu.value === "all"){
+                iconsPrintHtml (icon.family, icon.prefix, icon.name, icon.color, icon.type);
+            }
+        });  
+    }
+)
 
 
-function printHtml (family, prefix, name, color){
-    container.innerHTML += `
-    <div id="icon">
+function iconsPrintHtml (family, prefix, name, color, type){
+    containerGeneral.innerHTML += `
+    <div class="icon ${type}">
     <i class="${family} ${prefix}${name}" style="color: ${color};"></i>
     <div>${name}</div>
     </div>
